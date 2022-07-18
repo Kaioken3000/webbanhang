18e051971fd47ad3f92e6e00fed9ff57eea901b0
Kaioken3000 <nhutnam393@gmail.com>
2022-07-18 17:13:38 +0700

    new file:   src/main/java/com/mywebsite/webbanhang/admin/cart/CartAdminController.java 	modified:   src/main/java/com/mywebsite/webbanhang/client/cart/CartService.java 	modified:   src/main/resources/templates/admin/account_/account.html 	modified:   src/main/resources/templates/admin/account_/accountUpdate.html 	new file:   src/main/resources/templates/admin/cart_/cartAdmin.html 	new file:   src/main/resources/templates/admin/cart_/cartDetail.html 	modified:   src/main/resources/templates/admin/category_/category.html 	modified:   src/main/resources/templates/admin/category_/categorySearch.html 	modified:   src/main/resources/templates/admin/category_/categoryUpdate.html 	modified:   src/main/resources/templates/admin/dashboard.html 	modified:   src/main/resources/templates/admin/item_/item.html 	modified:   src/main/resources/templates/admin/item_/itemSearch.html 	modified:   src/main/resources/templates/admin/item_/itemUpdate.html 	modified:   src/main/resources/templates/registration.html

--- src/main/java/com/mywebsite/webbanhang/client/cart/CartService.java
+++ src/main/java/com/mywebsite/webbanhang/client/cart/CartService.java
@@ -3,6 +3,10 @@ package com.mywebsite.webbanhang.client.cart;
 import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -50,5 +54,12 @@ public class CartService {
     public Cart getCartById(long id){
         return cartRepository.findById(id).get();
     }
-    
+ 
+    public Page<Cart> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection){
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+			Sort.by(sortField).descending();
+		
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+		return this.cartRepository.findAll(pageable);
+    }  
 }
