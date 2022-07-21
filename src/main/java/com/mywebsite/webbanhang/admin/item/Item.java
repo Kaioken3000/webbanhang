3bd91d1af688bbe0eea3354d715c62a4895dd593
Kaioken3000 <nhutnam393@gmail.com>
2022-07-21 13:48:24 +0700

    modified:   src/main/java/com/mywebsite/webbanhang/admin/item/Item.java

--- src/main/java/com/mywebsite/webbanhang/admin/item/Item.java
+++ src/main/java/com/mywebsite/webbanhang/admin/item/Item.java
@@ -16,7 +16,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Cascade;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.mywebsite.webbanhang.admin.category.Category;
