3a6ea0f0f4b22347d9abffd77a4ba5107c0f90c3
Kaioken3000 <nhutnam393@gmail.com>
2022-07-23 11:11:30 +0700

    modified:   src/main/java/com/mywebsite/webbanhang/controller/CartController.java 	modified:   src/main/resources/templates/admin/cart_/cartAdmin.html 	modified:   src/main/resources/templates/client/index.html 	modified:   src/main/resources/templates/client/product-details.html 	modified:   src/main/resources/templates/client/shop.html

--- src/main/java/com/mywebsite/webbanhang/controller/CartController.java
+++ src/main/java/com/mywebsite/webbanhang/controller/CartController.java
@@ -1,6 +1,8 @@
 package com.mywebsite.webbanhang.controller;
 
 import java.security.Principal;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.mywebsite.webbanhang.model.Cart;
 import com.mywebsite.webbanhang.model.CustomOAuth2User;
+import com.mywebsite.webbanhang.model.Item;
 import com.mywebsite.webbanhang.model.User;
 import com.mywebsite.webbanhang.service.CartService;
 import com.mywebsite.webbanhang.service.UserService;
@@ -25,17 +28,17 @@ public class CartController {
     CartService cartService;
     UserService userService;
     HttpServletRequest request;
-    
+
     public CartController(CartService cartService, UserService userService, HttpServletRequest request) {
         this.cartService = cartService;
         this.userService = userService;
         this.request = request;
     }
 
-    public User getUser_(Authentication authentication){
+    public User getUser_(Authentication authentication) {
         Principal principal = request.getUserPrincipal();
         User user = userService.getUserByEmail(principal.getName());
-        if(user == null ){
+        if (user == null) {
             CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
             user = userService.getUserByEmail(oauthUser.getEmail());
         }
@@ -43,32 +46,65 @@ public class CartController {
     }
 
     @GetMapping("/client/cart")
-    public String showCartPage(Model model, Authentication authentication){
+    public String showCartPage(Model model, Authentication authentication) {
         User user = getUser_(authentication);
         model.addAttribute("listCarts", cartService.listCartByUserLoginId(user.getId()));
         return "/client/cart";
     }
 
     @PostMapping("/client/cart/addCart/{itemCart}")
-    public RedirectView addCart(@PathVariable long itemCart, @ModelAttribute("cart") Cart cart, Authentication authentication){
+    public RedirectView addCart(@PathVariable long itemCart, @ModelAttribute("cart") Cart cart,
+            Authentication authentication) {
         User user = getUser_(authentication);
 
-        cart.setUser(user);
-        cart.setHasAddCart(true);
-        cart.setHasBuy(false);
-        cartService.addCart(cart);
+        List<Cart> listCart = cartService.listCartByUserLoginId(user.getId());
+        Iterator<Cart> itr = listCart.iterator();
+        Cart c = new Cart();
+
+        //kiem tra da co trong cart hay chua
+        boolean kt = false;
+        long idCart = 0;
+        while (itr.hasNext()) {
+            c = (Cart) itr.next();
+            Iterator<Item> i = c.getItemCart().iterator();
+            while (i.hasNext()) {
+                Item it = i.next();
+                System.out.println(it.getId());
+                if (it.getId() == itemCart) {
+                    idCart = c.getId();
+                    kt=true;
+                    break;
+                }
+            }
+        }
+
+        
+        if (kt == true) {
+            Cart extingCart = cartService.getCartById(idCart);
+            extingCart.setId(idCart);
+            extingCart.setNumber(cart.getNumber());
+            extingCart.setUser(user);
+            cartService.updateCart(extingCart);
+        }
+        else{
+            cart.setUser(user);
+            cart.setHasAddCart(true);
+            cart.setHasBuy(false);
+            cartService.addCart(cart);
+        }
+
         String referer = request.getHeader("Referer");
         return new RedirectView(referer);
     }
 
     @GetMapping("/client/cart/deleteCart/{id}")
-    public String deleteCart(@PathVariable long id){
+    public String deleteCart(@PathVariable long id) {
         cartService.deleteCartById(id);
         return "redirect:/client/cart";
     }
 
     @GetMapping("/client/cart/deleteAllUserCart")
-    public String deleteAllUserCart(Authentication authentication){
+    public String deleteAllUserCart(Authentication authentication) {
         User user = getUser_(authentication);
 
         cartService.deleteAllCartByUserId(user.getId());
@@ -76,18 +112,15 @@ public class CartController {
     }
 
     @GetMapping("/client/cart/updateCart/{id}/item/{itemId}/number/{number}")
-    public String updateCart(@PathVariable("id") long id, @PathVariable("itemId") long itemId, 
-    @PathVariable("number") int number,  @ModelAttribute("listCarts") Cart cart ,
-    Authentication authentication){
+    public String updateCart(@PathVariable("id") long id, @PathVariable("itemId") long itemId,
+            @PathVariable("number") int number, @ModelAttribute("listCarts") Cart cart,
+            Authentication authentication) {
+        
+        User user = getUser_(authentication);
         Cart extingCart = cartService.getCartById(id);
         extingCart.setId(cart.getId());
         extingCart.setNumber(number);
-
-        User user = getUser_(authentication);
-        Cart c = cartService.getCartByItemIdAndUserId(user.getId(), itemId);
-        extingCart.setItemCart(c.getItemCart());
         extingCart.setUser(user);
-
         cartService.updateCart(extingCart);
 
         return "redirect:/client/cart";
